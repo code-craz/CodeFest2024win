e9216cc9859d4c4475afee4a8abe0b2e3759b964
code-craz <104960993+code-craz@users.noreply.github.com>
2024-01-20 12:02:35 +0530

    Read csv and created suggest function for interpreting

---
+++ AI_product.py
@@ -0,0 +1,19 @@
+#Import necessary module
+import pandas as pd 
+
+#Read csv file
+df= pd.read_csv('Cleaned_data_version2_sanath.csv')
+
+#Print index
+print("Printing index")
+print(df.keys)
+
+#Create function to suggest products
+def suggest_products(category, sub_category):
+    #Convert to lower case and remove trailing or leading whitespaces
+    category= category.lower().strip()
+    sub_category= sub_category.lower().strip()
+    print("Category given is ", category, "\n")
+    print("Sub category given is ", sub_category, "\n")
+    filtdf= df[(df['category'].str.lower().strip() == category & df['sub_category'].str.strip().str.lower())]
+    
